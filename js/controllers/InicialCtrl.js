bb46200861a97e5903e8f805fc05e26b44a197dd
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2018-02-26 09:50:04 -0300

    Tela inicial prototipo

--- js/controllers/InicialCtrl.js
+++ js/controllers/InicialCtrl.js
@@ -1,3 +1,84 @@
-app.controller('InicialCtrl', function() {
+app.controller('InicialCtrl', function($scope) {
+    $scope.topPopulares = [];
 
+    $scope.topPopulares.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
+        name: "Roundwood House"
+    });
+    $scope.topPopulares.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
+        name: "Castle Durrow"
+    });
+    $scope.topPopulares.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
+        name: "Slieve Bloom Bar"
+    });
+    $scope.topPopulares.push({
+        image: "https://buziosturismo.com/wp-content/uploads/cache/images/Cristo-Redentor-11/Cristo-Redentor-11-2432722836.jpg",
+        name: "Cristo Redentor"
+    });
+    $scope.topPopulares.push({
+        image: "https://http2.mlstatic.com/painel-em-lona-paisagem-paris-torre-eiffel-3x2te18-display-D_NQ_NP_387225-MLB25410769433_032017-F.jpg",
+        name: "Torre Eiffel"
+    });
+    $scope.topPopulares.push({
+        image: "http://s2.glbimg.com/YXuZNyYL3I2LJuQCs2fA79qnWE0=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/06/21/estatua.jpg",
+        name: "Estátua da Liberdade"
+    });
+
+    /* ---------------------------------------------------------------------------------------- */
+    $scope.topAvaliados = [];
+
+    $scope.topAvaliados.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
+        name: "Roundwood House"
+    });
+    $scope.topAvaliados.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
+        name: "Castle Durrow"
+    });
+    $scope.topAvaliados.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
+        name: "Slieve Bloom Bar"
+    });
+    $scope.topAvaliados.push({
+        image: "https://buziosturismo.com/wp-content/uploads/cache/images/Cristo-Redentor-11/Cristo-Redentor-11-2432722836.jpg",
+        name: "Cristo Redentor"
+    });
+    $scope.topAvaliados.push({
+        image: "https://http2.mlstatic.com/painel-em-lona-paisagem-paris-torre-eiffel-3x2te18-display-D_NQ_NP_387225-MLB25410769433_032017-F.jpg",
+        name: "Torre Eiffel"
+    });
+    $scope.topAvaliados.push({
+        image: "http://s2.glbimg.com/YXuZNyYL3I2LJuQCs2fA79qnWE0=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/06/21/estatua.jpg",
+        name: "Estátua da Liberdade"
+    });
+
+    /* ---------------------------------------------------------------------------------------- */
+    $scope.topCustoBeneficio = [];
+
+    $scope.topCustoBeneficio.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
+        name: "Roundwood House"
+    });
+    $scope.topCustoBeneficio.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
+        name: "Castle Durrow"
+    });
+    $scope.topCustoBeneficio.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
+        name: "Slieve Bloom Bar"
+    });
+    $scope.topCustoBeneficio.push({
+        image: "https://buziosturismo.com/wp-content/uploads/cache/images/Cristo-Redentor-11/Cristo-Redentor-11-2432722836.jpg",
+        name: "Cristo Redentor"
+    });
+    $scope.topCustoBeneficio.push({
+        image: "https://http2.mlstatic.com/painel-em-lona-paisagem-paris-torre-eiffel-3x2te18-display-D_NQ_NP_387225-MLB25410769433_032017-F.jpg",
+        name: "Torre Eiffel"
+    });
+    $scope.topCustoBeneficio.push({
+        image: "http://s2.glbimg.com/YXuZNyYL3I2LJuQCs2fA79qnWE0=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/06/21/estatua.jpg",
+        name: "Estátua da Liberdade"
+    });
 });
