3fd926c6deffec529f7fefe6ade592191313543c
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2018-02-27 15:45:19 -0300

    Front-end tela inicial atualizado

--- js/controllers/InicialCtrl.js
+++ js/controllers/InicialCtrl.js
@@ -1,14 +1,14 @@
 app.controller('InicialCtrl', function($scope) {
     $scope.topPopulares = [];
 
-    $scope.topPopulares.push({
+    /*$scope.topPopulares.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
         name: "Roundwood House"
     });
     $scope.topPopulares.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
         name: "Castle Durrow"
-    });
+    });*/
     $scope.topPopulares.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
         name: "Slieve Bloom Bar"
@@ -29,14 +29,14 @@ app.controller('InicialCtrl', function($scope) {
     /* ---------------------------------------------------------------------------------------- */
     $scope.topAvaliados = [];
 
-    $scope.topAvaliados.push({
+    /*$scope.topAvaliados.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
         name: "Roundwood House"
     });
     $scope.topAvaliados.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
         name: "Castle Durrow"
-    });
+    });*/
     $scope.topAvaliados.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
         name: "Slieve Bloom Bar"
@@ -57,14 +57,14 @@ app.controller('InicialCtrl', function($scope) {
     /* ---------------------------------------------------------------------------------------- */
     $scope.topCustoBeneficio = [];
 
-    $scope.topCustoBeneficio.push({
+    /*$scope.topCustoBeneficio.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
         name: "Roundwood House"
     });
     $scope.topCustoBeneficio.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
         name: "Castle Durrow"
-    });
+    });*/
     $scope.topCustoBeneficio.push({
         image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
         name: "Slieve Bloom Bar"
@@ -78,6 +78,34 @@ app.controller('InicialCtrl', function($scope) {
         name: "Torre Eiffel"
     });
     $scope.topCustoBeneficio.push({
+        image: "http://s2.glbimg.com/YXuZNyYL3I2LJuQCs2fA79qnWE0=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/06/21/estatua.jpg",
+        name: "Estátua da Liberdade"
+    });
+
+    /* ---------------------------------------------------------------------------------------- */
+    $scope.topCategoriasUsuario = [];
+
+    /*$scope.topCategoriasUsuario.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/07/d3/b5/84/roundwood-house.jpg",
+        name: "Roundwood House"
+    });
+    $scope.topCategoriasUsuario.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-w/04/ac/a0/02/castle-durrow.jpg",
+        name: "Castle Durrow"
+    });*/
+    $scope.topCategoriasUsuario.push({
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/03/00/74/4b/slieve-bloom-bar.jpg",
+        name: "Slieve Bloom Bar"
+    });
+    $scope.topCategoriasUsuario.push({
+        image: "https://buziosturismo.com/wp-content/uploads/cache/images/Cristo-Redentor-11/Cristo-Redentor-11-2432722836.jpg",
+        name: "Cristo Redentor"
+    });
+    $scope.topCategoriasUsuario.push({
+        image: "https://http2.mlstatic.com/painel-em-lona-paisagem-paris-torre-eiffel-3x2te18-display-D_NQ_NP_387225-MLB25410769433_032017-F.jpg",
+        name: "Torre Eiffel"
+    });
+    $scope.topCategoriasUsuario.push({
         image: "http://s2.glbimg.com/YXuZNyYL3I2LJuQCs2fA79qnWE0=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/06/21/estatua.jpg",
         name: "Estátua da Liberdade"
     });
