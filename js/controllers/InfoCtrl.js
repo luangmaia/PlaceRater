21f8022e2bd4358c4017d39c9525cf052d143ff6
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2018-03-05 16:33:59 -0300

    Finalização front-end base

--- js/controllers/InfoCtrl.js
+++ js/controllers/InfoCtrl.js
@@ -11,5 +11,18 @@ app.controller('InfoCtrl', function($scope) {
         categories: ['Monumento', 'Sítio Histórico']
     };
 
-
+    $scope.rates = [
+        {
+            Name: "luangmaia",
+            Stars: 5,
+            Price: 4,
+            Comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            Name: "leehdmgs",
+            Stars: 3,
+            Price: 2,
+            Comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+    ]
 });
